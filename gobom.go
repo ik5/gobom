71d93b32353e3f00b3854332eb7d6a166e877aef
Ido Kanner <idok@linesip.com>
2018-06-22 21:52:24 +0300

    Initial commit

---
+++ gobom.go
@@ -0,0 +1,156 @@
+/*
+Package gobom contains several methods to detect BOM type.
+
+BOM stands for Byte Order Mark. It is a standard by the Unicode organization
+to understand the type of encoding is standing in front of us, by placing non
+printable chars that explains if an encoding is UTF16 or UTF32, and what is the
+endian that it uses.
+The standard does not recommend to place a BOM to UTF8, but it supports that as
+well.
+
+This library was created for helping me detect if something contain a BOM, and
+that's it. It does not do anything else, and there is no plan for anything other
+then detecting it.
+
+How does the library works?
+
+The library can use io.Reader, and also "pure" byte slices and rune slices in
+order to detect the type of BOM.
+
+Please note:
+If a BOM is not detected, then it will return "Unknown".
+If a buffer is too small to detect BOM type it also returns "Unknown"
+*/
+package gobom
+
+import (
+	"bytes"
+	"io"
+)
+
+// BOM Headers to detect
+// The information is from: http://www.unicode.org/faq/utf_bom.html#BOM
+var (
+	UTF8Bom    = []byte{0xEF, 0xBB, 0xBF}
+	UTF16LEBom = []byte{0xFF, 0xFE}
+	UTF16BEBom = []byte{0xFE, 0xFF}
+	UTF32LEBom = []byte{0xFF, 0xFE, 0x00, 0x00}
+	UTF32BEBom = []byte{0x00, 0x00, 0xFE, 0xFF}
+)
+
+// BOMType holds the type of BOM that was detected
+type BOMType uint8
+
+// Enumeration of what type of BOM was found
+const (
+	Unknown BOMType = iota
+	UTF8
+	UTF16LE
+	UTF16BE
+	UTF32LE
+	UTF32BE
+)
+
+// Reader is an implementation for the io.Reader
+type Reader struct {
+	reader io.Reader
+	buffer []byte
+	err    error
+}
+
+// DetectBOMTypeFromBytes try to detect the type of BOM provided by a buffer in
+// a naive manner. It means that the detection is very simple but a bit costly
+// regarding the way it detects.
+//
+// The buffer must at least have 5 bytes, so from 2 - 4 bytes will be the BOM
+// if they do not exists, it returns Unknown
+func DetectBOMTypeFromBytes(buffer []byte) BOMType {
+	if len(buffer) < 5 {
+		return Unknown
+	}
+
+	// Naive checking for BOM based on size of BOM to validate.
+	// it's a bit slow
+
+	if bytes.HasPrefix(buffer, UTF16LEBom) {
+		return UTF16LE
+	} else if bytes.HasPrefix(buffer, UTF16BEBom) {
+		return UTF16BE
+	} else if bytes.HasPrefix(buffer, UTF8Bom) {
+		return UTF8
+	} else if bytes.HasPrefix(buffer, UTF32LEBom) {
+		return UTF32LE
+	} else if bytes.HasPrefix(buffer, UTF32BEBom) {
+		return UTF32BE
+	}
+
+	return Unknown
+}
+
+// IsUTF8BOM validate a buffer if it has UTF8 BOM, if buffer is too small it
+// return false
+func IsUTF8BOM(buffer []byte) bool {
+	if len(buffer) < len(UTF8Bom) {
+		return false
+	}
+
+	return buffer[0] == UTF8Bom[0] &&
+		buffer[1] == UTF8Bom[1] &&
+		buffer[3] == UTF8Bom[2]
+}
+
+// IsUTF16LEBOM validate a buffer if it has UTF16 Little Endian.
+// If the buffer is too small, it returns false.
+func IsUTF16LEBOM(buffer []byte) bool {
+	if len(buffer) < len(UTF16LEBom) {
+		return false
+	}
+
+	return buffer[0] == UTF16LEBom[0] && buffer[1] == UTF16LEBom[1]
+}
+
+// IsUTF16BEBOM validate a buffer if it has UTF16 big Endian.
+// If the buffer is too small, it returns false.
+func IsUTF16BEBOM(buffer []byte) bool {
+	if len(buffer) < len(UTF16BEBom) {
+		return false
+	}
+
+	return buffer[0] == UTF16BEBom[0] && buffer[1] == UTF16BEBom[1]
+}
+
+//IsUTF16BOM detects if a buffer contains any UTF16 BOM (big or little endian).
+func IsUTF16BOM(buffer []byte) bool {
+	return IsUTF16LEBOM(buffer) || IsUTF16BEBOM(buffer)
+}
+
+//IsUTF32LEBOM detects if a buffer contains UTF32 little endian.
+// If the buffer is too small, it returns false.
+func IsUTF32LEBOM(buffer []byte) bool {
+	if len(buffer) < len(UTF32LEBom) {
+		return false
+	}
+
+	return buffer[0] == UTF32LEBom[0] &&
+		buffer[1] == UTF32LEBom[1] &&
+		buffer[2] == UTF32LEBom[2] &&
+		buffer[3] == UTF32LEBom[3]
+}
+
+//IsUTF32BEBOM detects if a buffer contains UTF32 big endian.
+// If the buffer is too small, it returns false.
+func IsUTF32BEBOM(buffer []byte) bool {
+	if len(buffer) < len(UTF32BEBom) {
+		return false
+	}
+
+	return buffer[0] == UTF32BEBom[0] &&
+		buffer[1] == UTF32BEBom[1] &&
+		buffer[2] == UTF32BEBom[2] &&
+		buffer[3] == UTF32BEBom[3]
+}
+
+//IsUTF32BOM detects if a buffer is UTF32 BOM (either big or little endian).
+func IsUTF32BOM(buffer []byte) bool {
+	return IsUTF32LEBOM(buffer) || IsUTF32BEBOM(buffer)
+}
